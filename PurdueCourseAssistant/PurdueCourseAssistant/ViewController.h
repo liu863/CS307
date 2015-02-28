023cbbab6cf44650147de8a0d29cf3e30d8a7e19
SQuare <SQuare@pal-nat184-034-239.itap.purdue.edu>
2015-02-28 12:09:03 -0500

    set up

---
+++ PurdueCourseAssistant/PurdueCourseAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PurdueCourseAssistant
+//
+//  Created by SQuare on 2/28/15.
+//  Copyright (c) 2015 team_9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
