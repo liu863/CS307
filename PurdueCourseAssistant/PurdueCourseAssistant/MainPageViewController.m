61ba8c2fb2ac9793f3cb827c7387290b8eeab8d7
SQuare <SQuare@pal-nat184-040-232.itap.purdue.edu>
2015-04-23 21:55:42 -0400

    MainPage fixed

--- PurdueCourseAssistant/PurdueCourseAssistant/MainPageViewController.m
+++ PurdueCourseAssistant/PurdueCourseAssistant/MainPageViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainPageViewController.h"
+#import "ServerInfo.h"
 #import "Course.h"
 #import "User.h"
 
@@ -14,11 +15,25 @@
 
 @end
 
+CFReadStreamRef readStream;
+CFWriteStreamRef writeStream;
+
+NSInputStream * inputStream;
+NSOutputStream * outputStream;
+
+NSString * s;
+
 @implementation MainPageViewController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self initNetworkCommunication];
+    NSLog(@"flag 111111");
+    [self sendRequest:@"getclst|000"];
+    NSLog(@"request sent");
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,19 +41,90 @@
     // Dispose of any resources that can be recreated.
 }
 
+/***************************************************/
+// Client Implementation
+/***************************************************/
 
-- (IBAction)viewallCourse:(id)sender {
-    course.tags = @"000";
+- (void)initNetworkCommunication {
+    ServerInfo * server = [[ServerInfo alloc] init];
+    CFStringRef hostAddress = (__bridge CFStringRef)server.hostAddress;
+    int port = [server.port intValue];
+    NSLog(@"host = %@, port = %d", hostAddress, port);
+    CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, hostAddress, port, &readStream, &writeStream);
+    if(!CFWriteStreamOpen(writeStream)) {
+        NSLog(@"Error: writeStream");
+        return;
+    }
+    inputStream = (__bridge NSInputStream *)readStream;
+    outputStream = (__bridge NSOutputStream *)writeStream;
+    [inputStream setDelegate:self];
+    [outputStream setDelegate:self];
+    [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [inputStream open];
+    [outputStream open];
+    
+}
+
+
+-(void)stream:(NSStream *)stream handleEvent:(NSStreamEvent)eventCode {
+    switch (eventCode) {
+        case NSStreamEventHasSpaceAvailable: {
+            if(stream == outputStream){
+                NSLog(@"none\n");
+            }
+            break;
+        }
+        case NSStreamEventHasBytesAvailable: {
+            if(stream == inputStream) {
+                uint8_t buf[1024];
+                unsigned int len = 0;
+                len = [inputStream read:buf maxLength:1024];
+                if(len > 0) {
+                    NSMutableData* data=[[NSMutableData alloc] initWithLength:0];
+                    [data appendBytes: (const void *)buf length:len];
+                    s = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+                    
+                    /***************************************************/
+                    // Process Server Respond
+                    /***************************************************/
+                    NSLog(@"Main Page Respond received: %@", s);
+                    course.courseList = [[NSString alloc] initWithString: s];
+                    NSLog(@"Main Page courselist updated: %@", s);
+                    /***************************************************/
+                    // End
+                    /***************************************************/
+                    
+                }
+            }
+            break;
+        }
+        case NSStreamEventEndEncountered: {
+            NSLog(@"Stream closed\n");
+            break;
+        }
+        default: {
+            NSLog(@"Stream is sending an Event: %lu", eventCode);
+            break;
+        }
+    }
+}
+
+-(void)sendRequest: (NSString *) request{
+    NSString * tmp = [NSString stringWithFormat:@"%@\r\n", request];
+    NSData *data = [[NSData alloc] initWithData:[tmp dataUsingEncoding:NSASCIIStringEncoding]];
+    [outputStream write:[data bytes] maxLength:[data length]];
+    [outputStream close];
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
